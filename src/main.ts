14672623678ea5068d3bbdf87b7fd100de163d05
Deni Dwi Arta <denidarta@gmail.com>
2026-03-20 22:19:14 +0700

    add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 
 async function bootstrap() {
@@ -12,6 +13,15 @@ async function bootstrap() {
 			forbidNonWhitelisted: true,
 		})
 	);
+	const config = new DocumentBuilder()
+		.setTitle('API Documentation')
+		.setDescription('Belum ada deskripsi')
+		.setVersion('1.0')
+		.addBearerAuth()
+		.build();
+	const document = SwaggerModule.createDocument(app, config);
+	SwaggerModule.setup('api/docs', app, document);
+
 	await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();

d26c2bd38fce3e14e311508c1b87d05f2c0f9868
Deni Dwi Arta <denidarta@gmail.com>
2026-03-20 20:00:46 +0700

    Enhancement in auth dtos

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,14 +1,30 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import {
+	IsEmail,
+	IsNotEmpty,
+	IsString,
+	MinLength,
+	Matches,
+	IsOptional,
+} from 'class-validator';
 
 export class RegisterDto {
+	@IsNotEmpty()
 	@IsEmail()
 	email: string;
 
+	@IsNotEmpty()
 	@IsString()
-	@MinLength(6)
+	@Matches(
+		/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+		{
+			message:
+				'password must contain at least one uppercase letter, one lowercase letter, one number, and one special character',
+		}
+	)
 	password: string;
 
+	@IsOptional()
 	@IsString()
 	@MinLength(1)
-	name: string;
+	name?: string;
 }

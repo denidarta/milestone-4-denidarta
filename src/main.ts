98f2cf299ad6d62849b785a23730e8243b0bf8b4
Deni Dwi Arta <denidarta@gmail.com>
2026-04-18 09:56:56 +0700

    fix: add postinstall script to adapt vercel

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import helmet from 'helmet';
 import { ExpressAdapter } from '@nestjs/platform-express';
-import * as express from 'express';
+import express from 'express';
 
 const server = express();
 
